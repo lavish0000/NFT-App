58c2363f46f77298fe3fdacb63e71c7e50d52533
lavish0000 <lavick100@gmail.com>
2022-02-06 21:03:50 +0530

    Fix: send ethereum_address as string in seed migration

--- database/seedData/1643927632708_seed-nft-items.ts
+++ database/seedData/1643927632708_seed-nft-items.ts
@@ -9,11 +9,11 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
         nft_items 
         (title, description, image_url, created_by, owner, price, is_open_for_sale, ethereum_address) 
         VALUES 
-        ('NFT_item_1', 'NFT_item_1_description', 'https://picsum.photos/200/300', 'NFT_item_1_created_by', 'NFT_item_1_owner', '49', true, 0xB5d8593C11561CC105db3Da8a1A92D8Bd200199F),
-        ('NFT_item_2', 'NFT_item_2_description', 'https://picsum.photos/id/237/200/300', 'NFT_item_2_created_by', 'NFT_item_2_owner', '25', true, 0xB5d8593C11561CC105db3Da8a1A92D8Bd200199F),
-        ('NFT_item_3', 'NFT_item_3_description', 'https://picsum.photos/seed/picsum/200/300', 'NFT_item_3_created_by', 'NFT_item_3_owner', '50', false, 0xB5d8593C11561CC105db3Da8a1A92D8Bd200199F),
-        ('NFT_item_4', 'NFT_item_4_description', 'https://picsum.photos/200/300?grayscale', 'NFT_item_4_created_by', 'NFT_item_4_owner', '35', true, 0xB5d8593C11561CC105db3Da8a1A92D8Bd200199F),
-        ('NFT_item_5', 'NFT_item_5_description', 'https://picsum.photos/200/300/?blur=2', 'NFT_item_5_created_by', 'NFT_item_5_owner', '60', false, 0xB5d8593C11561CC105db3Da8a1A92D8Bd200199F)
+        ('NFT_item_1', 'NFT_item_1_description', 'https://picsum.photos/200/300', 'NFT_item_1_created_by', 'NFT_item_1_owner', '49', true, '0xB5d8593C11561CC105db3Da8a1A92D8Bd200199F'),
+        ('NFT_item_2', 'NFT_item_2_description', 'https://picsum.photos/id/237/200/300', 'NFT_item_2_created_by', 'NFT_item_2_owner', '25', true, '0xB5d8593C11561CC105db3Da8a1A92D8Bd200199F'),
+        ('NFT_item_3', 'NFT_item_3_description', 'https://picsum.photos/seed/picsum/200/300', 'NFT_item_3_created_by', 'NFT_item_3_owner', '50', false, '0xB5d8593C11561CC105db3Da8a1A92D8Bd200199F'),
+        ('NFT_item_4', 'NFT_item_4_description', 'https://picsum.photos/200/300?grayscale', 'NFT_item_4_created_by', 'NFT_item_4_owner', '35', true, '0xB5d8593C11561CC105db3Da8a1A92D8Bd200199F'),
+        ('NFT_item_5', 'NFT_item_5_description', 'https://picsum.photos/200/300/?blur=2', 'NFT_item_5_created_by', 'NFT_item_5_owner', '60', false, '0xB5d8593C11561CC105db3Da8a1A92D8Bd200199F')
     `)
 }
 
